d5439ff7237ab44d86c56dfe5bacf87e6a8ba054
Roman Glushach <7760934+EliFuzz@users.noreply.github.com>
2026-07-01 02:36:53 -0400

    clean up codex script

--- .stilla/codex_business.1h.sh
+++ .stilla/codex_business.1h.sh
@@ -8,6 +8,5 @@ _json_extract() {
 }
 
 response=$(echo $(curl -s "https://chatgpt.com/backend-api/wham/usage" -H "Authorization: Bearer $(_json_extract access_token < "$path")" -H "Accept: application/json") | tr '\n' ' ')
-[ "$(printf '%s\n' "$response" | _json_extract plan_type)" = business ] || exit 0
 
 echo '{"title":"Codex '"$(printf '%s\n' "$response" | _json_extract used_percent | sed 's/\..*//')"'%","items":[{"item_type":"text","title":"'"$(printf '%s\n' "$response" | _json_extract used | sed 's/\..*//')"' / '"$(printf '%s\n' "$response" | _json_extract limit | sed 's/\..*//')"' ('"$(date -r "$(printf '%s\n' "$response" | _json_extract reset_at)" '+%Y-%m-%d %I:%M %p' 2>/dev/null)"')"}]}'
